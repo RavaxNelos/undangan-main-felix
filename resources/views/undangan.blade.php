f7adf8f2f2ca4a57db48e968b18b8ded3aa46f73
RavaxNelos <nrava741@gmail.com>
2025-01-30 23:47:49 +0700

    first commit

---
+++ resources/views/undangan.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gambar Responsif</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        <div class="image-container">
+            <img src="Frame1.jpg" alt="Frame 1" class="img-fluid">
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
